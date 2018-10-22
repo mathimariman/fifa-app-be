2ff927baaa21b10d747da67c6ad8d0c44b0eba96
Mathias Mariman <mathias.mariman@gmail.com>
2018-10-22 16:40:35 +0200

    Getting players from EA api

--- routes/players.js
+++ routes/players.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
 router.get('/search', (req, res) => {
-    res.send('Searching:' + req.query.term);
+    if (req.query.term) {
+        const searchTerm = JSON.stringify({name: req.query.term});
+        console.log(searchTerm);
+        axios.get('https://www.easports.com/fifa/ultimate-team/api/fut/item?jsonParamObject=' + searchTerm)
+            .then(function (response) {
+                res.send(response.data.items);
+            })
+            .catch(function (error) {
+                console.log(error);
+                res.status(500);
+            });
+    } else {
+        res.status(400).send('No term specified')
+    }
 });
 
 module.exports = router;
