cb23fd566a23922a1edebb9a4af002fe12793d63
Kenneth Van den Berghe <kenneth.vandenberghe@optis.be>
2018-11-05 17:52:09 +0100

    prices refactor

--- routes/prices.js
+++ routes/prices.js
@@ -3,45 +3,42 @@ const router = express.Router();
 const axios = require('axios');
 const passport = require('passport');
 
-router.get('/', passport.authenticate('basic', {session: false}), (req, res) => {
-    const playerIdsParameter = req.query.playerIds;
-    if (playerIdsParameter) {
-        const playerIds = playerIdsParameter.split(',');
-        axios.get('https://www.futbin.com/19/playerPrices' + generateUrl(playerIds.slice()))
-            .then(function (response) {
-                const output = {};
-                playerIds.forEach((playerId) => {
-                    if (response.data[playerId]) {
-                        output[playerId] = convertToNumber(response.data[playerId].prices.ps.LCPrice);
-                    } else {
-                        output[playerId] = 0;
-                    }
-                });
-                res.send(output);
-            })
-            .catch(function (error) {
-                console.log(error);
-                res.status(500);
-            });
-    } else {
-        res.status(400).send('No player id specified')
-    }
+router.get('/', (req, res) => {
+  const playerIdsParameter = req.query.playerIds;
+  if (!playerIdsParameter) {
+    return res.status(400).send('No player id specified');
+  }
+
+  const playerIds = playerIdsParameter.split(',');
+  axios
+    .get(`https://www.futbin.com/19/playerPrices${generateUrl(playerIds)}`)
+    .then(function(response) {
+      const output = playerIds.reduce((acc, playerId) => {
+        return {
+          ...acc,
+          [playerId]: response.data[playerId]
+            ? convertToNumber(response.data[playerId].prices.ps.LCPrice)
+            : 0
+        };
+      }, {});
+      res.send(output);
+    })
+    .catch(function(error) {
+      console.log(error);
+      res.status(500);
+    });
 });
 
-generateUrl = (playerIds) => {
-    if (playerIds.length === 1) {
-        return '?player=' + playerIds[0];
-    } else {
-        return '?player=' + playerIds.splice(0, 1) + '&all_versions=' + playerIds.join(',');
-    }
+generateUrl = playerIds => {
+  if (playerIds.length === 1) {
+    return '?player=' + playerIds[0];
+  } else {
+    return `?player=${playerIds[0]}&all_versions=${playerIds.slice(1).join(',')}`;
+  }
 };
 
-convertToNumber = (playerPrice) => {
-    if (isNaN(playerPrice)) {
-        return +playerPrice.replace(/,/g, '');
-    } else {
-        return 0;
-    }
-}
+convertToNumber = playerPrice => {
+  return isNaN(playerPrice) ? +playerPrice.replace(/,/g, '') : 0;
+};
 
 module.exports = router;
